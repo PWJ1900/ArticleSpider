6a624ecf7f09ab471a60d53e60ba352756e8b0bd
buripan <940731286@qq.com>
2020-11-12 10:29:28 +0800

    Scrapy use

--- ArticleSpider/items.py
+++ ArticleSpider/items.py
@@ -10,3 +10,17 @@ class ArticlespiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class JoBoleArticleItem(scrapy.Item):
+  title = scrapy.Field()#他的所有的字段都是定义的这个类型(可以理解为这里的字段都会逐一的保存放到数据库之中)
+  time = scrapy.Field()
+  url = scrapy.Field()
+  url_object_id = scrapy.Field()
+  front_image_url = scrapy.Field()
+  front_image_path = scrapy.Field()
+  praise_nums = scrapy.Field()
+  comment_nums = scrapy.Field()
+  fav_nums = scrapy.Field()
+  tags = scrapy.Field()
+  content = scrapy.Field()
+
--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -7,6 +7,9 @@ import scrapy#scrapy是异步io框架 没有多线程，没有引入消息队列
 from scrapy import Request
 
 
+from ArticleSpider.utils import common
+from ArticleSpider.items import JoBoleArticleItem
+
 class JobboleSpider(scrapy.Spider):
   name = 'jobbole'
   allowed_domains = ['news.cnblogs.com']
@@ -42,31 +45,53 @@ class JobboleSpider(scrapy.Spider):
   def parse_detail(self, response):
     match_re =re.match(".*?(\d+)", response.url)#先判断url有没有id
     if match_re:
+      post_id = match_re.group(1)  # 此处提取post_id来给下面的的id赋值
+
+      article_Item = JoBoleArticleItem()
+
+
       title = response.css("#news_title a::text").extract_first("")
       time = response.css("#news_info .time::text").extract_first("")
+      match_re = re.match(".*?(\d+.*)", time)
+      if match_re:
+        time = match_re.group(1)
       content = response.css("#news_content").extract()[0]
       tag_list = response.css(".news_tags a::text").extract()
+
+      # title = response.xpath("//*[@id='news_title']//a/text()").extract_first("")
+      # time = response.xpath("//*[@id='news_info']//*[@class='time']/text()").extract_first("")
+      # content = response.xpath("//*[@id='news_content']").extract()[0]
+      # tag_list = response.xpath("//*[@class='news_tags']//a/text()").extract()
       tags = ",".join(tag_list)
 
 
-      post_id = match_re.group(1)  # 此处提取post_id来给下面的的id赋值
+
       # html = requests.get(parse.urljoin(response.url, "/NewsAjax/GetAjaxNewsInfo?contentId={}".format(post_id)))#注意因为requests是同步的而这里用的是一个异步的方法，所以换异步的方法实现,    得加/
       # j_data = json.loads(html.text)
 
+      article_Item["title"] = title#这里面一定要确保key是在item里面存在的
+      article_Item["time"] = time
+      article_Item["tags"] = tags
+      article_Item["content"] = content
+      article_Item["url"] = response.url
+      article_Item["front_image_url"] = response.meta.get("front_image_url","")#可以通过此方法获得传递过来的图片值
+
       #用yeid把上方法换成异步
-      yield Request(url=parse.urljoin(response.url, "/NewsAjax/GetAjaxNewsInfo?contentId={}".format(post_id)), callback=self.parse_nums)
-
-
-
-
-
-
-      pass
+      yield Request(url=parse.urljoin(response.url, "/NewsAjax/GetAjaxNewsInfo?contentId={}".format(post_id)),
+                    meta={"article_item": article_Item}, callback=self.parse_nums)
 
   def parse_nums(self, response):
+    article_item = response.meta.get("article_item", "")
     j_data = json.loads(response.text)
-    praise_nums  = j_data["DiggCount"]
+    praise_nums = j_data["DiggCount"]
     fav_nums = j_data["TotalView"]
     comment_nums = j_data["CommentCount"]
-    pass
+
+    article_item["praise_nums"] = praise_nums
+    article_item["fav_nums"] = fav_nums
+    article_item["comment_nums"] = comment_nums
+    article_item["url_object_id"] = common.get_md5(article_item["url"])
+
+    yield article_item
+
 
